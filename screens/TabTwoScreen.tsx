7320492e348532695dc03e5c7ea5e8e83c896381
Burton <blo@Burtons-MBP.wireless.yale.internal>
2022-04-10 06:49:00 -0400

    meep mop

--- screens/TabTwoScreen.tsx
+++ screens/TabTwoScreen.tsx
@@ -1,15 +1,32 @@
-import { StyleSheet } from 'react-native';
-
+import { StyleSheet, ScrollView, SafeAreaView} from 'react-native';
+import HomePageCompanyView from '../components/HomePageCompanyView'
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
+import { LAYOUTS } from '../constants/Layouts';
+import SearchBar from '../components/SearchBar';
+import { Entypo } from '@expo/vector-icons';
 
-export default function TabTwoScreen() {
+export default function TabTwoScreen({ navigation }) {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab Two</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabTwoScreen.tsx" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <ScrollView>
+        <View style={styles.scrollContainer}>
+          <SearchBar/>
+          <View style={styles.titleView}>
+            <Text style={styles.title}>
+              Discover: 
+            </Text>
+            <Text style={{...styles.title, color: "#2ABD22"}}>
+              Just Launched
+            </Text>
+            <Entypo name="menu" size={29} style={{color: "#2ABD22"}}/>
+          </View>
+          <HomePageCompanyView navigation={navigation}/>
+          <HomePageCompanyView navigation={navigation}/>
+          <HomePageCompanyView navigation={navigation}/>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
@@ -18,14 +35,20 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: 'white'
+  },
+  scrollContainer: {
+    alignItems: 'center',
+    justifyContent: 'flex-start'
+  },
+  titleView: {
+    flexDirection: 'row'
   },
   title: {
-    fontSize: 20,
+    fontSize: 23,
     fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+    fontFamily: 'Avenir',
+    marginBottom: LAYOUTS.getWidth(10),
+    marginRight: LAYOUTS.getWidth(7)
   },
 });
