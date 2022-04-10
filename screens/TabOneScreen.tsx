a5fc2e4f4d7e0cbf4bc773134a97665d259e9970
Burton <blo@Burtons-MBP.wireless.yale.internal>
2022-04-10 03:58:04 -0400

    dojheohefiohgep9

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -3,18 +3,23 @@ import { StyleSheet, ScrollView, SafeAreaView} from 'react-native';
 import EditScreenInfo from '../components/EditScreenInfo';
 import HomePageCompanyView from '../components/HomePageCompanyView'
 import { Text, View} from '../components/Themed';
+import { LAYOUTS } from '../constants/Layouts';
 import { RootTabScreenProps } from '../types';
 
 export default function TabOneScreen({ navigation }) {
   return (
-  <ScrollView>
     <SafeAreaView style={styles.container}>
-      <Text style={styles.title}>Recommended For You</Text>
-      <HomePageCompanyView navigation={navigation}/>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <ScrollView>
+        <View style={styles.scrollContainer}>
+          <Text style={styles.title}>Recommended for you</Text>
+          <HomePageCompanyView navigation={navigation}/>
+          <HomePageCompanyView navigation={navigation}/>
+          <HomePageCompanyView navigation={navigation}/>
+        </View>
+      </ScrollView>
     </SafeAreaView>
-  </ScrollView>
   );
+  //     <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
 }
 
 const styles = StyleSheet.create({
@@ -22,11 +27,18 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: 'white'
+  },
+  scrollContainer: {
+    alignItems: 'center',
+    justifyContent: 'flex-start'
   },
   title: {
     fontSize: 28,
     fontWeight: 'bold',
-    fontFamily: 'Avenir'
+    fontFamily: 'Avenir',
+    marginTop: LAYOUTS.getWidth(12),
+    marginBottom: LAYOUTS.getWidth(10)
   },
   separator: {
     marginVertical: 30,
